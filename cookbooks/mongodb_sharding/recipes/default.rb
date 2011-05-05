8cf6b70e3f9ec66b80be112a82b357d2b020aaf2
PavelT <paveltyk@gmail.com>
2011-05-05 17:15:26 +0300

    Setup sharding.

--- cookbooks/mongodb_sharding/recipes/default.rb
+++ cookbooks/mongodb_sharding/recipes/default.rb
@@ -8,5 +8,6 @@ if ['app_master','app','solo'].include? @node[:instance_role]
   require_recipe "mongodb_sharding::install"
   require_recipe "mongodb_sharding::configure_mongos"
   require_recipe "mongodb_sharding::start_mongos"
+  require_recipe "mongodb_sharding::setup_sharding"
 end
 
